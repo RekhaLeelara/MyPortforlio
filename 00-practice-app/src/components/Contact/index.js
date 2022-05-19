5cbc08bdd2ac3b71dc1f89ec8ec60d0882bc8b6e
RekhaLeelara <62070645+RekhaLeelara@users.noreply.github.com>
2022-05-19 12:25:47 -0500

    updated the contact page to handle the form object to store data

--- 00-practice-app/src/components/Contact/index.js
+++ 00-practice-app/src/components/Contact/index.js
@@ -1,21 +1,27 @@
 //importing the necessary files
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { validateEmail } from '../../utils/helpers';
 
 
 //To display the contents displayed in the Contact page
 
-function Contact(){
+function Contact() {
 
-  const [formState, setFormState] = useState({name: '', email:'', message:''})
-  const[errorMessage, setErrorMessage] = useState('');
-  const{name, email, message} = formState;
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' })
+  const [errorMessage, setErrorMessage] = useState('');
+  const [messageSent, setMessageSent] = useState('')
+  const { name, email, message } = formState;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!errorMessage){
-      setFormState({name: '', email:'', message:''})
-      console.log('Form', formState);
+    if (!errorMessage) {
+
+      let userData = {
+        name: document.querySelector('.name').value,
+        email: document.querySelector('.email').value,
+        message: document.querySelector('.message').value,
+      }
+      setMessageSent('Message Has Sent!')
     }
   }
 
@@ -34,43 +40,44 @@ function Contact(){
         setErrorMessage('');
       }
     }
-    if (!errorMessage){
-      setFormState({...formState, [e.target.name]: e.target.value});
-      console.log("setFormStateemail", formState);
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      // console.log("setFormStateemail", formState);
     }
   };
 
-//   function handleSubmit(e) {
-//     e.preventDefault();
-// }
+  //   function handleSubmit(e) {
+  //     e.preventDefault();
+  // }
 
 
-    return(
-        <section>
-        <h1 data-testid="h1tag">Contact</h1>
-        <form id="contact-form" onSubmit={handleSubmit}>
-        <div className = "field">
+  return (
+    <section>
+      <h1 data-testid="h1tag">Contact</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div className="field">
           <label htmlFor="name">Name: </label>
-          <input type="text" name="name"  className = "name" defaultValue={name} onBlur={handleChange} />
+          <input type="text" name="name" className="name" defaultValue={name} onBlur={handleChange} />
         </div>
 
-        <div className = "field">
+        <div className="field">
           <label htmlFor="email">Email address: </label>
-          <input type="email" name="email" className = "email" defaultValue={email} onBlur={handleChange}/>
+          <input type="email" name="email" className="email" defaultValue={email} onBlur={handleChange} />
         </div>
-        <div className = "field">
+        <div className="field">
           <label htmlFor="message">Message: </label>
-          <textarea name="message" rows="15" columns="20"  className = "message" defaultValue={message} onBlur={handleChange}/>
+          <textarea name="message" rows="15" columns="20" className="message" defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
           <div>
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button data-testid="button" className = "button" type="submit">Submit</button>
-        </form>
-        </section>
-    );
+        <button data-testid="button" className="button" type="submit">Submit</button>
+        <h3>{messageSent}</h3>
+      </form>
+    </section>
+  );
 }
 
 //Exporting the Contact page contents
